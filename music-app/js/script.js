451f4d323a9d481b93aadeaa8f4e666ebc8f41ee
ndhoangofficial <71540709+ndhoangofficial@users.noreply.github.com>
2021-11-28 12:59:10 +0700

    Update script.js

--- music-app/js/script.js
+++ music-app/js/script.js
@@ -183,7 +183,7 @@ for (let i = 0; i < allMusic.length; i++) {
                   <p>${allMusic[i].artist}</p>
                 </div>
                 <span id="${allMusic[i].src}" class="audio-duration">3:40</span>
-                <audio class="${allMusic[i].src}" src="https://github.com/ndhoangofficial/cdn/raw/main/music-app/songs/music-1.mp3"></audio>
+                <audio class="https://github.com/ndhoangofficial/cdn/raw/main/music-app/songs/music-1.mp3" src="https://github.com/ndhoangofficial/cdn/raw/main/music-app/songs/music-1.mp3"></audio>
               </li>`;
   ulTag.insertAdjacentHTML("beforeend", liTag); //inserting the li inside ul tag
 
